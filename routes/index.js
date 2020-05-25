185f3ae0e128f77eb1cab35f8546d4ba236cf3d3
Paulina Arnesson <paulinaarnesson@hotmail.com>
2020-05-25 19:40:39 +0200

    Started upp and installed most of the packages i will need. And cleaned up the existing files

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET / */
+router.get('/', (req, res) => {
+  res.send({ status: 'success' });
+});
+
+module.exports = router;
